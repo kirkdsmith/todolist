b961872571f6ede64030764d5396c7917e1a031f
Kirk Smith <kirk@paraconcepts.com>
2020-12-21 14:51:05 -0500

    Updates

---
+++ precache-manifest.ab0c20fe935ba77c06fa6931f72fcb61.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d0955eca0fbafb95ac61fbe66d3bc0",
+    "url": "/github.io/todolist/index.html"
+  },
+  {
+    "revision": "96ce79134edbc3dea4e0",
+    "url": "/github.io/todolist/static/css/2.cfb9cd3c.chunk.css"
+  },
+  {
+    "revision": "96ce79134edbc3dea4e0",
+    "url": "/github.io/todolist/static/js/2.a72f538e.chunk.js"
+  },
+  {
+    "revision": "5278e78823efff555b02",
+    "url": "/github.io/todolist/static/js/main.1c8e8ccd.chunk.js"
+  },
+  {
+    "revision": "fc44e4344f87cd65b02a",
+    "url": "/github.io/todolist/static/js/runtime-main.938b7270.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/github.io/todolist/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/github.io/todolist/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/github.io/todolist/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/github.io/todolist/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/github.io/todolist/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/github.io/todolist/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/github.io/todolist/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/github.io/todolist/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/github.io/todolist/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/github.io/todolist/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/github.io/todolist/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/github.io/todolist/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/github.io/todolist/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/github.io/todolist/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/github.io/todolist/static/media/fa-solid-900.c2801fb4.svg"
+  }
+]);
